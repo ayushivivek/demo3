a8129ea7cf00a6022ce24110970e0b4055eeaae4
Ayshu012 <ayushisoni012@gmail.com>
2023-08-17 19:48:28 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,215 @@
 import React, { useRef, useState } from "react";
-import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
-import "./App.css";
+import {
+  ArrowBackIos as MdArrowBackIos,
+  ArrowForwardIos as MdArrowForwardIos,
+} from "@material-ui/icons";
+import {
+  Button,
+  Typography,
+  IconButton,
+  Paper,
+  Divider,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    margin: "50px 40px 10px 40px",
+  },
+  header: {
+    backgroundColor: "#333",
+    padding: "10px 15px",
+    display: "flex",
+    alignItems: "center",
+    overflow: "Hidden",
+  },
+  scrollContainer: {
+    overflowX: "auto",
+    display: "flex",
+    flexGrow: 1,
+  },
+  scrollItem: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    cursor: "pointer",
+  },
+  active: {
+    backgroundColor: "#ff9d1a",
+    color: "#fff",
+  },
+  sidebar: {
+    width: "100%",
+    marginBottom: "20px",
+    boxShadow: "none",
+  },
+  content: {
+    flex: "1 1",
+    display: "flex",
+    flexDirection: "column",
+  },
+  pagination: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: theme.spacing(2),
+  },
+  pageButton: {
+    backgroundColor: "#ffdcb5",
+    border: "none",
+    cursor: "pointer",
+    transition: "background-color 0.2s",
+    borderRadius: "10px",
+    padding: "10px",
+  },
+  dd: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "30px",
+    "& h6": {
+      fontSize: "16px",
+      fontWeight: "bold",
+    },
+    "& h5": {
+      fontSize: "2em",
+      position: "relative",
+      marginBottom: "20px",
+      fontWeight: "bold",
+      textAlign: "center",
+      "& span": {
+        content: "",
+        position: "absolute",
+        margin: "0 auto",
+        left: "0",
+        right: "0",
+        backgroundColor: "orange",
+        width: "140px",
+        height: "1px",
+        bottom: "-12px",
+      },
+    },
+  },
+  mainContainer: {
+    display: "flex",
+    flex: "1",
+    "@media (max-width: 767px)": {
+      flexDirection: "column",
+    },
+  },
+  ibps: {
+    textShadow: "2px 2px 2px #ff9415",
+    color: "#333333",
+    fontSize: "34px",
+    textAlign: "center",
+    marginBottom: "10px",
+  },
+  outbtn: {
+    width: "100%",
+    textAlign: "center",
+    marginBottom: "20px",
+    marginTop: "35px",
+    color: "white",
+    padding: "10px 15px",
+    fontSize: "16px",
+    margin: "0px 5px 0px 5px",
+    backgroundColor: "#ff9415",
+    borderRadius: "20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  outbtnP: {
+    color: "white",
+    padding: "10px 15px",
+    fontSize: "16px",
+    margin: "0px 5px 0px 5px",
+    backgroundColor: "#ff9415",
+    borderRadius: "20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sidebar: {
+    width: "200px",
+    backgroundColor: "#20324b",
+    color: "#fff",
+    padding: "20px 18px",
+    boxShadow: "2px 0 5px rgba(0, 0, 0, 0.1)",
+    boxSizing: "border-box",
+    "@media (max-width: 767px)": {
+      width: "100%",
+      marginBottom: "20px",
+    },
+  },
+  subheading: {
+    color: "#fff",
+    fontSize: "16px",
+    textAlign: "center",
+    marginBottom: "10px",
+  },
+  cards: {
+    padding: "0px 20px",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+    "@media (max-width: 767px)": {
+      padding: "0px",
+    },
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: "20px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    borderRadius: "5px",
+    textAlign: "center",
+    width: "calc(25% - 55px)",
+    "@media (max-width: 992px)": {
+      width: "calc(33.33% - 55px)",
+    },
+    "@media (max-width: 767px)": {
+      width: "calc(50% - 50px)",
+    },
+  },
+  scrollContainer: {
+    margin: "10px 0 0",
+    flexGrow: "1",
+    overflowX: "auto",
+    whiteSpace: "nowrap",
+    scrollbarWidth: "none",
+    "&::-webkit-scrollbar": {
+      width: 0,
+    },
+  },
+  scrollItem: {
+    display: "inline-block",
+    color: "white",
+    padding: "10px 15px",
+    fontSize: "16px",
+    margin: "0px 5px 0px 5px",
+    backgroundColor: "#243a52",
+    borderRadius: "20px",
+    "&:last-child": {
+      borderRight: "none",
+    },
+  },
+  scrollBtn: {
+    backgroundColor: "#fff",
+    color: "gray",
+    border: "none",
+    padding: "9px 9px",
+    cursor: "pointer",
+    borderRadius: "50%",
+    transition: "background-color 0.2s",
+    display: "flex",
+    justifyContent: "center",
+    "&:hover": {
+      backgroundColor: "#777",
+    },
+  },
+}));
 
 function App() {
   const [selectOpt, setselectOpt] = useState("Option 1");
@@ -11,12 +220,17 @@ function App() {
   const itemsPerPage = 12;
   const [currentPage, setCurrentPage] = useState(1);
   const totalPages = Math.ceil(allCards.length / itemsPerPage);
+  const classes = useStyles();
 
   // Logic to get the items for the current page
   const currentItems = allCards.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
+
+  const handleOption = (value) => {
+    setselectOpt(value);
+  };
 
   const scroll = (direction) => {
     if (scrollRef.current) {
@@ -27,30 +241,31 @@ function App() {
       }
     }
   };
-  const handleOption = (value) => {
-    setselectOpt(value);
-  };
 
   return (
-    <div className="container">
-      <div className="dd">
-        <h1 className="zeromargin explore">Explore By Exams</h1>
-        <h1 className="zeromargin popular">
-          <span>Most Popular Exams</span>
-        </h1>
+    <div className={classes.container}>
+      <div className={classes.dd}>
+        <Typography variant="h6">Explore By Exams</Typography>
+        <Typography variant="h5">
+          Most Popular Exams<span></span>
+        </Typography>
       </div>
-      <div className="header">
-        <button className="scroll-btn" onClick={() => scroll("left")}>
+      <div className={classes.header}>
+        <IconButton
+          className={classes.scrollBtn}
+          onClick={() => scroll("left")}
+        >
           <MdArrowBackIos />
-        </button>
-        <div className="scroll-container" ref={scrollRef}>
+        </IconButton>
+        <div className={classes.scrollContainer} ref={scrollRef}>
           {Array(20)
             .fill()
             .map((_, i) => (
               <div
+                elevation={2}
                 key={i}
-                className={`scroll-item ${
-                  selectOpt === `Option ${i + 1}` ? "active" : ""
+                className={`${classes.scrollItem} ${
+                  selectOpt === `Option ${i + 1}` ? classes.active : ""
                 }`}
                 onClick={() => handleOption(`Option ${i + 1}`)}
               >
@@ -58,46 +273,46 @@ function App() {
               </div>
             ))}
         </div>
-        <button className="scroll-btn" onClick={() => scroll("right")}>
+        <IconButton
+          className={classes.scrollBtn}
+          onClick={() => scroll("right")}
+        >
           <MdArrowForwardIos />
-        </button>
+        </IconButton>
       </div>
-      <div className="main-content">
-        <div className="sidebar">
-          <div className="ibps-rrb">IBPS RRB</div>
-          <div className="sub-heading">CBO</div>
-
-          <div className="out-btn">
-            <p>Out</p>
-          </div>
-
-          <div className="ibps-rrb">3330+</div>
+      <Divider style={{ margin: "20px 0" }} />
+      <div className={classes.mainContainer}>
+        <div className={classes.sidebar}>
+          <Typography className={classes.ibps}>IBPS RRB</Typography>
+          <Typography className={classes.subheading}>CBO</Typography>
+          <Button variant="outlined" className={classes.outbtn}>
+            Out
+          </Button>
+          <Typography className={classes.ibps}>3330+</Typography>
         </div>
-        <div className="content">
-          <div className="cards">
+        <div className={classes.content}>
+          <div className={classes.cards}>
             {currentItems.map((card, i) => (
-              <div key={i} className="card">
+              <Paper className={classes.card} key={i}>
                 <img
-                  style={{ width: "100%" }}
                   src="https://www.freepnglogos.com/uploads/sbi-logo-png/sbi-logo-sbi-symbol-meaning-history-and-evolution-11.png"
-                  width="200"
+                  width="100%"
                   alt="sbi logo sbi symbol meaning history and evolution"
                 />
-                <p>SBI Logo</p>
-              </div>
+                <Typography align="center">SBI Logo</Typography>
+              </Paper>
             ))}
           </div>
         </div>
       </div>
-      <div className="pagination">
+      <div className={classes.pagination}>
         {[...Array(totalPages).keys()].map((number) => (
-          <button
+          <Button
             key={number}
-            className={`page-button ${
-              currentPage === number + 1 ? "active" : ""
-            }`}
+            variant="contained"
+            className={currentPage === number + 1 ? classes.active : ""}
             onClick={() => setCurrentPage(number + 1)}
-          ></button>
+          />
         ))}
       </div>
     </div>
